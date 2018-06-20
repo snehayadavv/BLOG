fd3fd11c974cfd49c14daf793314ff82c55384ed
Sneha <snehayadavv7@gmail.com>
2018-06-20 15:46:21 +0000

    edit_and_update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var mongoose = require("mongoose");
 var express = require("express");
 var app = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //MONGOOSE CONFIG
 var blogSchema = new mongoose.Schema({
@@ -73,6 +75,35 @@ app.get("/blogs/:id",function(req,res){
      }); 
 });
 
+
+//EDIT 
+app.get("/blogs/:id/edit",function(req, res) {
+    Blog.findById(req.params.id,function(err,foundBlog){
+       if(err)
+       {
+           res.redirect("/blogs");
+       }
+       else{
+           res.render("edit",{blog: foundBlog});
+       }
+    });
+});
+
+
+//UPDATE
+app.put("/blogs/:id",function(req,res){
+    //Blog.findByIdAndUpdate(id,newdata,callback)
+    Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
+        if(err)
+        {
+            res.redirect("/blogs");
+        }
+        else {
+            res.redirect("/blogs/"+req.params.id);
+        }
+    });
+    
+});
 
 app.listen(process.env.PORT,process.env.IP,function(){
    console.log("connected");  
